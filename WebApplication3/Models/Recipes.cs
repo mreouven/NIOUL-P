4210fed1a160d919ac7757f817e063b29614a6d7
Nadin <nadin051092@gmail.com>
2019-04-04 14:55:44 +0300

    nadin Recipes Models

---
+++ WebApplication3/Models/Recipes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication3.Models
+{
+    public class Recipes
+    {
+        public string milky { get; set; }
+        public string fleshy { get; set; }
+        public string desserts { get; set; }
+        public string Parve { get; set; }
+    }
+}
