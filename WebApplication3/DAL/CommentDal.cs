7ec9472dbcc49ecc0ff8058d5f2c79b78ecb61da
hodayaliberty <34811687+hodayaliberty@users.noreply.github.com>
2019-04-16 20:53:33 +0300

    sp2oda

--- WebApplication3/DAL/CommentDal.cs
+++ WebApplication3/DAL/CommentDal.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication3.Models;
+
+namespace WebApplication3.DAL
+{
+    public class CommentDal: DbContext
+    {
+
+        public DbSet<Comment> Comments { get; set; }
+            
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Comment>().ToTable("tblComment");
+        }
+    }
+}
--- WebApplication3/DAL/UsersDal.cs
+++ WebApplication3/DAL/UsersDal.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication3.Models;
+
+namespace WebApplication3.DAL
+{
+    public class UsersDal : DbContext
+    {
+        public DbSet<Users> User { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Users>().ToTable("tblUser");
+        }
+    }
+}
