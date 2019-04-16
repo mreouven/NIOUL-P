35414a2fc38d4196213fbd0357ec2336a1083ba4
Nadin <nadin051092@gmail.com>
2019-04-16 20:45:30 +0300

    sp@2

--- WebApplication3/Models/Comment.cs
+++ WebApplication3/Models/Comment.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Comment
+    {
+        [Key]
+        [Required]
+        public string id { get; set; }
+
+
+        [Required]
+        public string idReceips { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string message { get; set; }
+        [Required]
+        public DateTime date { get; set; }
+
+    }
+}
