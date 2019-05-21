69146a30144a1a9a459607463b94a8ed2cb71136
Nadin <nadin051092@gmail.com>
2019-05-21 17:24:46 +0300

    na

---
+++ WebApplication3/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication3
+{
+    public class BundleConfig
+    {
+        // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/js/jquery.validate*"));
+
+            // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
+            // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/js/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/js/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.css",
+                      "~/css/camera.css",
+                      "~/css/elastislide.css",
+                      "~/css/font-awesome.css",
+                      "~/css/form.css",
+                      "~/css/grid.css",
+                      "~/css/style.css",
+                      "~/css/superfish.css",
+                      "~/css/ie.css",
+                      "~/css/touchTouch.css",
+                      "~/css/reset.css",
+                      "~/css/slider.css"
+
+                      ));
+        }
+    }
+}
