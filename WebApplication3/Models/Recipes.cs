2d03c4e7ed4e77ef451fb1eeb67db07dc5d310f9
shilat bitton <shilatbitton@gmail.com>
2019-05-21 16:42:21 +0300

    oundp

---
+++ WebApplication3/Models/Recipes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication3.Models
+{
+    public class Recipes
+    {
+
+        [Required]
+        [Key]
+        public string Id { get; set; }
+        [Required]
+
+        public string user { get; set; }
+
+        [Required]
+
+        public string Name { get; set; }
+        [Required]
+
+        public string types { get; set; }
+        [Required]
+
+        public string tempsdepreparation { get; set; }
+        [Required]
+
+        public string tempsdecuisson { get; set; }
+        [Required]
+
+        public string difficulty { get; set; }
+        [Required]
+
+        public string nombrePers { get; set; }
+        [Required]
+
+        public string ingredients { get; set; } //List
+        [Required]
+
+        public string explication { get; set; } //List
+
+
+    }
+}
